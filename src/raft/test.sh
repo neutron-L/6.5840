f6005f3c0cfc244fe02d847582eecfa4e70decf1
rda <2055017970@qq.com>
2024-11-03 09:14:09 +0800

    feat(lab3): modify test shell

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -6,6 +6,20 @@
 # do go test -race -run 3B; 
 # done 
 
-for i in {0..1000}; 
-do go test -race -run 3C; 
+
+
+for i in {0..100}; 
+do 
+ go test -run 3C >> 3C.log
+ go test -run 3D >> 3D.log 
+done 
+
+
+for i in {0..100}; 
+do go test -run 3A >> 3A.log; 
+done 
+
+
+for i in {0..100}; 
+do go test -run 3B >> 3B.log; 
 done 
